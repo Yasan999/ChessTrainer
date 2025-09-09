d856a741cb4ebaaed6ba7a0141cc345b3f3d9af3
Yasan999 <yachess08@gmail.com>
2025-09-09 10:04:35 +0300

    в

--- src/main/java/com/ctrainer/Application.java
+++ src/main/java/com/ctrainer/Application.java
@@ -4,7 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class Application {
@@ -14,6 +18,10 @@ public class Application {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        String idForEncode = "bcrypt";
+        Map<String, PasswordEncoder> encoders = new HashMap<>();
+        encoders.put(idForEncode, new BCryptPasswordEncoder());
+        encoders.put("noop", NoOpPasswordEncoder.getInstance());
+        return new DelegatingPasswordEncoder(idForEncode, encoders);
     }
 }
